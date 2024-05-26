f0879be9bc819a171f1ce7a043eb326a406e1927
mmbalbas <mmbalbasgolf@gmail.com>
2024-05-26 10:38:43 +0200

    Creacion de modelos, migraciones y controladores

---
+++ database/migrations/2024_05_26_081029_create_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->text('description')->nullable();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->integer('priority')->default(1); // 1: Low, 2: Medium, 3: High
+            $table->date('due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
